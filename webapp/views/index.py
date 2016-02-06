de053c95228fec9ebfa658e78be0929513166e74
Anirudha Bose <ani07nov@gmail.com>
2016-02-06 17:34:38 +0530

    New route for badges

--- webapp/views/index.py
+++ webapp/views/index.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from flask import redirect, Response
+from flask import redirect, Response, render_template
 
 from webapp import webapp
 from webapp.utils.check import RepoScanner
@@ -9,7 +9,25 @@ from webapp.utils.check import RepoScanner
 import json
 
 @webapp.route('/api/<owner>/<repo>')
-def index(owner, repo):
+def api(owner, repo):
     r = RepoScanner(owner, repo)
     result = r.get_results()
     return Response(json.dumps(result, indent=4), mimetype="application/json")
+
+@webapp.route('/<owner>/<repo>')
+def index(owner, repo):
+    r = RepoScanner(owner, repo)
+    results = r.get_results()
+    return render_template(
+        'index.html',
+        results=results
+    )
+
+@webapp.route('/badge/<owner>/<repo>')
+def badge(owner, repo):
+    r = RepoScanner(owner, repo)
+    results = r.get_results()
+
+    for each in results:
+        if each['status'] is False and each['severity'] is 2:
+            return redirect("https://img.shields.io/badge/repodoctor-Error-red.svg")
